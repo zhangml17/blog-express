eae8202e3267b182eb8c6d10572e0ddf50854a93
zhangml17 <meilongyx@163.com>
2019-11-07 23:04:55 +0800

    update: access-log

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+const fs = require('fs')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session')
@@ -18,7 +19,25 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
+
+const ENV = process.env.NODE_ENV
+if(ENV !== 'production') {
+  // 开发环境 / 测试环境
+  app.use(logger('dev'))
+}else{
+  // 线上环境
+
+  const filename = path.resolve(__dirname, 'logs', 'access.log')
+  const writeStream = fs.createWriteStream(filename, {
+    flags:'a'
+  })
+
+  app.use(logger('combined', {
+    stream: writeStream
+  }))
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
